3cbafdf4b13e55170d38f3af039317bebe258480
Shadab Ali <shadabali604@gmail.com>
2020-05-09 17:57:58 +0530

    project almost finish

---
+++ server.js
@@ -0,0 +1,40 @@
+
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var cors = require('cors');
+
+// Setup empty JS object to act as endpoint for all routes
+projectData = {};
+
+// Require Express to run server and routes
+app.get('/GetApi', (req, res) => {
+res.send(projectData);
+});
+
+ app.post('/api', function(req, res) {
+     const newData = req.body;
+     projectData = {...projectData};
+     res.status(200).send(projectData);
+ });
+// Start up an instance of app
+
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+
+// Cors for cross origin allowance
+app.use(cors())
+
+// Initialize the main project folder
+app.use(express.static('website'));
+
+
+// Setup Server
+
+app.listen(3000, function(){
+    console.log('I am listening');
+});
---
+++ website/app.js
@@ -0,0 +1,57 @@
+import { response } from "express";
+
+/* Global Variables */
+let gen = document.getElementById('generate');
+let feel = document.getElementById('feelings').value;
+
+// Create a new date instance dynamically with JS
+let d = new Date();
+let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();   
+
+
+
+const apiKey = '04e27c99302e8b6d91b215783b8e4ff3';
+let city = 'ghaziabad';
+
+
+
+gen.addEventListener('click', function(){
+    let zip = document.getElementById('zip').value;
+    let url = `https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?zip=${zip},IN&appid=${apiKey}`;
+    let res = fetch(url)
+    .then(data => data.json())
+     .then((JsonResponse) => {
+         let temp = JsonResponse.main.temp;
+        console.log(JsonResponse.main.temp)
+    
+     });
+
+
+
+    return fetch('http://localhost:3000/api',{
+    method: "POST",
+    body:JSON.stringify({
+        temperature:temp,
+        Date: newDate,
+        feeling: feel
+    }),
+    headers:{
+        "Content-type": "application/json"
+    }
+    })
+    .then(JsonResponse => {
+        return JsonResponse
+    }).then(function({temperature, Date, feeling}) {
+        document.getElementById('temp') = temperature + 'Temp';
+        document.getElementById('date') = Date + 'Date';
+        document.getElementById('content') = feeling + 'Feel'
+    })
+    .catch(function(error) {
+        console.log('Request failed', error)
+      })
+})
+
+
+
+
+ 
