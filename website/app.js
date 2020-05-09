1e293914ad90ed31bb3515c9a8c4f0f0399c2ddd
Shadab Ali <shadabali604@gmail.com>
2020-05-09 23:03:55 +0530

    added new method

--- website/app.js
+++ website/app.js
@@ -1,54 +1,48 @@
  
+ 
 /* Global Variables */
 let gen = document.getElementById('generate');
-let feel = document.getElementById('feelings').value;
-
+let feel = document.getElementById('feelings');
+let zip = document.getElementById('zip');
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();   
-
-
-
 const apiKey = '04e27c99302e8b6d91b215783b8e4ff3';
-let city = 'ghaziabad';
-
-
 
 gen.addEventListener('click', function(){
-    let zip = document.getElementById('zip').value;
-    let url = `https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?zip=${zip},IN&appid=${apiKey}`;
-    let res = fetch(url)
-    .then(data => data.json())
-     .then((JsonResponse) => {
-         let temp = JsonResponse.main.temp;
-        console.log(JsonResponse.main.temp)
-    
-     });
+ let url = `https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?zip=${zip.value},IN&appid=${apiKey}`;
+fetch(url).then(function(response) {
+    return response.json();
+}).then(function(response) {
+  let temperature = response.main.temp;
+  console.log(temperature);
+  console.log(newDate);
+  console.log(feel.value);
+  return fetch('http://localhost:3000/api', {
+      method: "POST",
+      body: JSON.stringify({
+          temperature:temperature,
+          date: newDate,
+          feeling:feel.value
+      }),
+      headers:{
+          "Content-type": "application/json"
+      }
 
-
-
-    return fetch('http://localhost:3000', { mode: 'no-cors'},{
-    method: "POST",
-    body:JSON.stringify({
-        temperature:temp,
-        Date: newDate,
-        feeling: feel
-    }),
-    headers:{
-        "Content-type": "application/json"
-    }
-    })
-    .then(JsonResponse => {
-        return JsonResponse
-    }).then(function({temperature, Date, feeling}) {
-        document.getElementById('temp') = temperature + 'Temp';
-        document.getElementById('date') = Date + 'Date';
-        document.getElementById('content') = feeling + 'Feel'
-    })
-    .catch(function(error) {
-        console.log('Request failed', error)
-      })
+  })
+ }).then(function(response) {
+     console.log(response.json());
+     return response.json();
+ }).then(function({temperature, feeling, date}) {
+     console.log(temperature);
+     console.log(feeling);
+document.getElementById('date').innerHTML = date + 'Date'
+document.getElementById('temp').innerHTML = temperature +'Temp'
+document.getElementById('content').innerHTML = feeling +'feeling'
 })
+})
+
+
 
 
 
